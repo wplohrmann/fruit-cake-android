17899d9ae05c3be2fa85aa8dcbb563ad3ddd13da
William Lohrmann <wlohrmann@oni.bio>
2020-07-25 16:06:48 +0100

    Implement firestore updates for items

--- src/ShoppingLists/List.tsx
+++ src/ShoppingLists/List.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import firestore from '@react-native-firebase/firestore';
 
 import {
   FlatList,
@@ -11,24 +12,55 @@ import {
   Divider,
 } from 'react-native-paper';
 
-import { UniqueShoppingItem } from './Types';
+import { UniqueShoppingItem, ShoppingItem } from './Types';
 
+function toggleItemComplete(item: UniqueShoppingItem) {
+  const newMode = (item.mode === "sought") ? "bought" : "sought";
+  let oldItem = {...item};
+  delete oldItem.id;
+  oldItem.mode = newMode;
+  const newItem: ShoppingItem = oldItem;
+  firestore()
+    .collection("ShoppingItems")
+    .doc(item.id)
+    .set(newItem)
+    .then(() => console.log(newItem));
+}
 
 function renderItem(item: UniqueShoppingItem) {
-    return (
-      <Card onPress={() => Alert.alert("Done!")} onLongPress={() => Alert.alert("Long press!")} >
-        <Divider/>
-        <Card.Title title={item.name} left={(props: any) => <Avatar.Icon {...props} icon="folder"/>} />
-        <Divider/>
-      </Card>
-    );
+  let icon: string;
+  switch (item.mode) {
+    case "sought":
+      icon = "checkbox-blank-outline";
+      break;
+    case "bought":
+      icon = "checkbox-marked";
+      break;
+    case "not-in-store":
+      icon = "cart-off";
+      break;
+    case "removed":
+      icon = "crop-square";
+      break;
+  }
+  return (
+    <Card onPress={() => toggleItemComplete(item)} onLongPress={() => Alert.alert("Long press!")} >
+      <Divider/>
+      <Card.Title title={item.name} left={(props: any) => <Avatar.Icon {...props} icon={icon}/>} />
+      <Divider/>
+    </Card>
+  );
 }
 
 function keyExtractor(item: UniqueShoppingItem, _: number): string {
     return item.id;
 }
 
-export const List = (props: {items: UniqueShoppingItem[]}) => {
+interface Props {
+  items: UniqueShoppingItem[];
+}
+
+export const List = (props: Props) => {
   return (
     <FlatList style={{flex: 1}} data={props.items} renderItem={obj => renderItem(obj.item)} keyExtractor={keyExtractor}/>
   );
