79fed77b8955ce3571309e874ddaff96568c141c
William Lohrmann <wlohrmann@oni.bio>
2020-07-25 10:55:47 +0100

    Set up infrastructure for shopping list renames

--- src/ShoppingLists/Header.tsx
+++ src/ShoppingLists/Header.tsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { Dimensions } from 'react-native';
 import {
   Appbar,
   Menu,
+  Divider,
 } from 'react-native-paper';
 import firestore from '@react-native-firebase/firestore';
 
 import {ShoppingList} from './Types';
+import {RenameShoppingList} from './Header/RenameShoppingList';
 
 interface Props {
   user_id: string;
@@ -50,30 +53,37 @@ export const Header = (props: Props) => {
     return subscriber;
   }, []);
 
+
   if (props.shoppingList === undefined) {
     return <Appbar.Header><Appbar.Content title="Loading..."/></Appbar.Header>;
   }
 
   return (
+    <>
     <Appbar.Header>
-      <Menu
-        visible={visible}
-        onDismiss={() => setVisible(false)}
-        anchor={<Appbar.Content title={props.shoppingList.name} onPress={() => setVisible(true)}/>}>
-        {shoppingLists.map((item: ShoppingList, index: number) => <Menu.Item title={item.name} key={index} onPress={() => {
-            props.setShoppingList(shoppingLists[index]);
-            setVisible(false);
-          }
-        } />)}
-      </Menu>
-
-      <Menu
-        visible={moreVisible}
-        onDismiss={() => setMoreVisible(false)}
-        anchor={<Appbar.Action icon="dots-vertical" onPress={() => setMoreVisible(true)}/>}>
-        <Menu.Item title="Log out" onPress={() => props.logOut()} />
-      </Menu>
-
+      <Appbar.Content title={props.shoppingList.name} onPress={() => setVisible(true)}/>
+      <Appbar.Action icon="dots-vertical" onPress={() => setMoreVisible(true)}/>
     </Appbar.Header>
+    <Menu
+      visible={moreVisible}
+      onDismiss={() => setMoreVisible(false)}
+      anchor={{x: Dimensions.get("window").width, y: 0}}>
+      <RenameShoppingList shoppingList={props.shoppingList} hideMenu={() => setMoreVisible(false)}/>
+      <Menu.Item title="New Shopping List" onPress={() => {}} />
+      <Divider/>
+      <Menu.Item title="Log out" onPress={() => props.logOut()} />
+    </Menu>
+    <Menu
+      visible={visible}
+      onDismiss={() => setVisible(false)}
+      anchor={{x: 0, y: 0}}>
+      {shoppingLists.map((item: ShoppingList, index: number) => <Menu.Item title={item.name} key={index} onPress={() => {
+          props.setShoppingList(shoppingLists[index]);
+          setVisible(false);
+        }
+      } />)}
+    </Menu>
+    </>
+
   );
 };
---
+++ src/ShoppingLists/Header/RenameShoppingList.tsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import {
+  TextInput,
+  Menu,
+  Portal,
+  Button,
+  Dialog,
+} from 'react-native-paper';
+
+import { ShoppingList } from './Types';
+
+interface Props {
+  shoppingList: ShoppingList;
+  hideMenu: any;
+}
+
+export const RenameShoppingList = (props: Props) => {
+  const [visible, setVisible] = useState(false);
+  const [renameInput, setRenameInput] = useState("");
+  
+  const handleRename = () => {
+    console.log("Shopping list renamed to " + JSON.stringify(renameInput));
+    dismiss();
+  };
+
+  const dismiss = () => {
+    setVisible(false);
+    props.hideMenu();
+  };
+
+  return (
+    <>
+      <Menu.Item title="Rename Shopping List" onPress={() => {
+        setVisible(true);
+      }}/>
+      <Portal>
+        <Dialog visible={visible} onDismiss={dismiss}>
+          <Dialog.Title>Rename {props.shoppingList.name}</Dialog.Title>
+          <TextInput placeholder="New name" value={renameInput} onChangeText={v => setRenameInput(v)}/>
+          <Dialog.Actions>
+            <Button onPress={handleRename}>Rename</Button>
+            <Button onPress={dismiss}>Cancel</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </>
+  );
+};
