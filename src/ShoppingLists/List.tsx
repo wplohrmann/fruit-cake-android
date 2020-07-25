0c847f42afd3e378da2489320a0b13353f3b41d3
William Lohrmann <wlohrmann@oni.bio>
2020-07-25 14:51:53 +0100

    Use id as unique key

--- src/ShoppingLists/List.tsx
+++ src/ShoppingLists/List.tsx
@@ -8,18 +8,18 @@ import {
   Text
 } from 'react-native-paper';
 
-import { ShoppingItem } from './Types';
+import { UniqueShoppingItem } from './Types';
 
 
-function renderItem(item: ShoppingItem) {
+function renderItem(item: UniqueShoppingItem) {
     return <Text>{item.name}</Text>;
 }
 
-function keyExtractor(item: ShoppingItem, index: number): string {
-    return item.name + index;
+function keyExtractor(item: UniqueShoppingItem, _: number): string {
+    return item.id;
 }
 
-export const List = (props: {items: ShoppingItem[]}) => {
+export const List = (props: {items: UniqueShoppingItem[]}) => {
   return (
     <FlatList style={{flex: 1}} data={props.items} renderItem={obj => renderItem(obj.item)} keyExtractor={keyExtractor}/>
   );
